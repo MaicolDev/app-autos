ab43e770e8ec3530ca190ab2ae88b7804862d737
Maicol <maicolfranco800@gmail.com>
2023-02-10 14:55:11 -0300

    bugs arreglados

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'appdjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-
-    )
+    'default': {dj_database_url.config(default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600)
         
     }
 }
