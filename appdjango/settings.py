53228df2e7685c7a69ed8137e112e9eb1f2c9310
Maicol <maicolfranco800@gmail.com>
2023-02-22 22:46:36 -0300

    bdpostgreconectada

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -80,11 +80,17 @@ WSGI_APPLICATION = 'appdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+        #'default': dj_database_url.config(
+            #default='postgresql://postgres:postgres@localhost:5432/appdjango',
+            #conn_max_age=600)
+    #}
+
+"postgres://pruebadb_ekf7_user:4kEkIYQXB4CBo4RtE3u2HoWfvA2lkc5W@dpg-cfrbsthgp3jo1dpbb32g-a.oregon-postgres.render.com/pruebadb_ekf7"
+
 DATABASES = {
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/appdjango',
-            conn_max_age=600)
-    }
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 
--- appdjango/urls.py
+++ appdjango/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from autos import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +32,6 @@ urlpatterns = [
     path("nuevoAuto/<int:auto_id>",views.auto_detalle, name="auto_detalle"),
     
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
