10874908fae92a1bb40b32dbd66ca7ab7511048f
Maicol <maicolfranco800@gmail.com>
2023-02-25 11:22:08 -0300

    correcciones tres

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -88,12 +88,12 @@ DATABASES = {
             conn_max_age=600)
 }
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #DATABASES = {
         #'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
