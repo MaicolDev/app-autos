3315bb356ab75e2063feab5cad816d402146361a
Maicol <maicolfranco800@gmail.com>
2023-02-25 11:43:36 -0300

    arreglos uno

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'appdjango.wsgi.application'
             #conn_max_age=600)
 #s}
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #DATABASES = {
         #'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
